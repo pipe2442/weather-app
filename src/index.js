4d87203804dcd329e20d4849e37efe3dd3252b74
pipe2442 <felipeangulo2442@gmail.com>
2021-06-28 16:44:42 -0500

    Add api consult to weather app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const sum = (a, b) => {
-    return a + b;
-}
+const city = 'barranquilla'
 
-console.log(sum(2, 2));
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=96f35486a037092415de2038db8e1138`, {mode: 'cors'})
+.then(function(response) {
+return response.json();
+}).then(function(response) {
+    console.log(response.main.temp - 273.15);
+}); 
