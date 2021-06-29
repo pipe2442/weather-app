792746a94f224827891a077a0365824d016d04ce
pipe2442 <felipeangulo2442@gmail.com>
2021-06-29 17:58:49 -0500

    Add time and styles page

--- src/modules/card.js
+++ src/modules/card.js
@@ -6,10 +6,25 @@ const displayWeather = (city, icon, temp, description, country, main) => {
     const div = document.createElement('div')
     div.setAttribute("id", "weatherCard");
     const src = `https://openweathermap.org/img/w/${icon}.png`
+
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    let time = new Date();
+    let minutes = time.getMinutes();
+    let hour = time.getHours();
+    
+
+    today = mm + '/' + dd + '/' + yyyy;
+    time = hour + ':' + minutes
  
     div.innerHTML = `
     <h2> ${city} </h2>
     <h2> ${country} </h2>
+    <h3> ${today} </h3>
+    <h3> ${time} </h3>
     <h3> ${temp} </h3>
     <p> ${main} </p>
     <p> ${description} </p>
