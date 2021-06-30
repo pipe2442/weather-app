6186ee0365c671c687e3bb83bd09c88162aa1bd5
pipe2442 <felipeangulo2442@gmail.com>
2021-06-30 14:54:49 -0500

    Add random background selector onclick - add toggle for temp in celcious and farenheit

--- src/modules/apiRequest.js
+++ src/modules/apiRequest.js
@@ -9,13 +9,28 @@ const request = () => {
     .then((response) => response.json())
     .then((data) => {
       const { icon } = data.weather[0];
-      const temp = (data.main.temp - 273.15).toFixed();
+      const { temp } = data.main;
       const { description } = data.weather[0];
       const { main } = data.weather[0];
       const { name } = data;
       const { country } = data.sys;
 
       displayWeather(name, icon, temp, description, country, main);
+
+      const cel = document.getElementById('option1');
+      const fa = document.getElementById('option2');
+      const cel2 = document.getElementById('cel2');
+      const fa2 = document.getElementById('fa2');
+      const showCel = () => {
+        fa2.style.visibility = 'hidden';
+        cel2.style.visibility = 'visible';
+      };
+      const showFa = () => {
+        cel2.style.visibility = 'hidden';
+        fa2.style.visibility = 'visible';
+      };
+      cel.onclick = showCel;
+      fa.onclick = showFa;
     });
 };
 
--- src/modules/card.js
+++ src/modules/card.js
@@ -17,18 +17,41 @@ const displayWeather = (city, icon, temp, description, country, main) => {
   today = `${mm}/${dd}/${yyyy}`;
   time = `${hour}:${minutes}`;
 
+  function random(min, max) {
+    const number = Math.random() * (max - min) + min;
+    return Math.trunc(number);
+  }
+  const number = random(1, 8);
+  const body = document.getElementById('bd');
+  const tempcel = temp - 273.15;
+  const tempfa1 = tempcel * 9;
+  const tempfa = tempfa1 / 5 + 32;
+  body.style.backgroundImage = `url(../../dist/assets/images/bg${number}.jpg)`;
+
   div.innerHTML = `
+  
+    <div class="btn-group btn-group-toggle d-flex justify-content-center toggleMargin" data-toggle="buttons">
+      <label class="btn btn-success">
+        <input type="radio" name="options" id="option1" autocomplete="off" checked> Celcious
+      </label>
+      <label class="btn btn-success">
+        <input type="radio" name="options" id="option2" autocomplete="off"> Faranheit
+      </label>
+    </div>
+
     <div class="text-white text-center card-weather card-box">
         <h2> ${city} </h2>
         <h2> ${country} </h2>
         <h3> ${today} </h3>
         <h3> ${time} </h3>
-        <h3> ${temp}°C </h3>
+        <h1 id="cel2" class="position"> ${Math.trunc(tempcel)}°C </h1>
+        <h1 id="fa2" class="fa position"> ${Math.trunc(tempfa)}°F </h1>
         <p> ${main} </p>
         <p> ${description} </p>
         <img src="${src}" alt="icon"">
     </div>
     `;
+
   if (dom.hasChildNodes()) { dom.removeChild(document.getElementById('weatherCard')); }
   dom.appendChild(div);
 };
