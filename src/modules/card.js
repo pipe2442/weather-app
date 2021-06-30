0283e6958e28e51bad8e1dfca2e88a62bd6cd0f8
pipe2442 <felipeangulo2442@gmail.com>
2021-06-29 20:50:30 -0500

    remove comments

--- src/modules/card.js
+++ src/modules/card.js
@@ -7,7 +7,7 @@ const displayWeather = (city, icon, temp, description, country, main) => {
 
   let today = new Date();
   const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
   const yyyy = today.getFullYear();
 
   let time = new Date();
