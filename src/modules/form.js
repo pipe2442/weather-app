bd656201b15d7266dd927ca0b36832b442e8bad7
pipe2442 <felipeangulo2442@gmail.com>
2021-06-30 16:02:31 -0500

    Check linters

--- src/modules/form.js
+++ src/modules/form.js
@@ -3,7 +3,7 @@ const dataForm = () => {
 
   form.innerHTML = `
     <h1 class="text-white text-center card-weather m-5"> Free Weather App </h1>
-    <h4 class="text-white text-center card-weather m-5"> Look for your city </h4>
+    <h4 class="text-white text-center card-weather m-5"> Look for your city. </h4>
     <form class="d-flex justify-content-center m-5">
         <input type="text" class="form-control" id ="userCity" placeholder="City"></input>
         <button type="button" class="btn btn-success" id="formBtn" >Submit</button>
